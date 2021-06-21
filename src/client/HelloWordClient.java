c9beb6eaaae22c1e23324243f5104458cd12f75b
cumpa_000 <cumpanasoiu.ui>
2021-06-21 20:22:35 +0300

    bookstore

---
+++ src/client/HelloWordClient.java
@@ -0,0 +1,25 @@
+package client;
+
+import org.hibernate.Session;
+import util.HibernateUtil;
+import entity.Message;
+
+public class HelloWordClient {
+
+    public static void main(String[] args) {
+
+        try {
+        Session session = HibernateUtil.getSessionFactory().openSession();
+        session.beginTransaction();
+
+        Message message = new Message("Hello Word with Hibernate and JPA Annotation");
+
+        session.save(message);
+
+        session.getTransaction().commit();
+        session.close();
+        }catch(NullPointerException npe){
+            System.out.println();
+        }
+    }
+}
